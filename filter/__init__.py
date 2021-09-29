35c34aad27fa75f43f9a6a6c7fb582e2c2a6c3af
mogoh <mv@mogoh.de>
2021-09-29 21:16:00 +0200

    Ignore words.json in git and add words.example.json as fallback

--- filter/__init__.py
+++ filter/__init__.py
@@ -1,12 +1,22 @@
 import json
 from log import get_logger
+import os
+
 
 l = get_logger(__name__)
+words_default = 'filter/words.example.json'
+words_custom = 'filter/words.json'
 
-def get_words() -> dict:
-    with open('filter/words.json', 'r') as f:
-        return json.loads(f.read())
-    
+
+def get_words() -> dict[str, str]:
+    if os.path.isfile(words_custom):
+        with open(words_custom, 'r') as f:
+            return json.loads(f.read())
+    else:
+        with open(words_default, 'r') as f:
+            return json.loads(f.read())
+
+
 def parse_word(word: str) -> str:
     return words.get(word, word).lower()
 
@@ -25,7 +35,6 @@ def parse_sentence(sentence: str) -> str:
     for word in words_in_s:
         new_words.append(parse_word(word))
     return ' '.join(new_words)
-
 
 
 words = get_words()
